f9f5c0b41289d1c63a4a0f5c951575290a737bf1
John <johnreysecoya@gmail.com>
2023-12-22 21:47:22 +0800

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+"use strict";
+import "./style.css";
+import todo from "./todoView.js";
+
+// const click = document.querySelector(".todo");
+// const rightSideBar = document.querySelector(".sidebar-right");
+
+// click.addEventListener("click", function (e) {
+//   rightSideBar.classList.toggle("inactive");
+// });
+
+todo.updateScreen();
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  //   optimization: {
+  //     runtimeChunk: "single",
+  //   },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "To DO List",
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
